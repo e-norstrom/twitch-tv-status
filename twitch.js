a53289fa3327fefe7a34cd2106a508055343c454
Home <elias.norstromm@gmail.com>
2018-05-07 23:05:04 +0100

    css update

--- twitch.js
+++ twitch.js
@@ -1,83 +1,62 @@
 
-
-
-//1. User Story: I can see whether Free Code Camp is currently streaming on Twitch.tv.
-// 2. User Story: I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel. 
-// 3. User Story: if a Twitch user is currently streaming, I can see additional details about what they are streaming.
-
-// Notes:
-
-// I can click a tab to display all channels
-// I can click a tab to see all online channels
-// I can click a tab to see all offline channels
-
-// Requests: /users/user, channels/channel
-// and /streams/stream
-
 $(document).ready(function() {
   console.log('Ready');
 
+  let updateColorClass = function(buttonId) {
+    let inactiveButtons = $(buttonId).siblings();
+    let activeButton = $(buttonId);
+    inactiveButtons.removeClass('active').addClass('inactive');
+    activeButton.removeClass('inactive').addClass('active');
+  };
 
   $('#all').click(function() {
     $('li').show();
+    updateColorClass(this);
   });
 
   $('#online').click(function() {
-    $('.blue').hide();
-    $('.green').show();
+    $('.grey').hide();
+    $('.purple').show();
+    updateColorClass(this);
   });
 
   $('#offline').click(function() {
-    $('.green').hide();
-    $('.blue').show();
+    $('.purple').hide();
+    $('.grey').show();
+    updateColorClass(this);
   });
 
-
-  let ul = document.getElementById('list');
-  let listIdNumber = 1;
+  let ul = $('#list');
 
   const USERS = ["OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "ESL_SC2", "noobs2ninjas", "epicenter_ru"];
 
-  //const ACTION = ["channels/", "users/", "streams/"];
-
   USERS.forEach(function(user) {
-    let li = $('').attr('id', listIdNumber);
 
     $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + user +  '?callback=?',  function(data) {
-      let linkHolder = document.createElement('a');
-      let link = data.url;
-      linkHolder.setAttribute('href', link);
-      linkHolder.setAttribute('target', '_blank');
-      //$(li).append(linkHolder);
+      let url = data.url;
+      let link = "href=" + url + ' ' + 'target="_blank"';
       console.log(data);
       console.log(link);
 
       $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?',  function(data) {
-          let streamActive = data.stream;
-          if (streamActive === null) {
-          li = $('<li>' + user + '</br>' + 'Status - Offline</li>');
-          $(li).addClass('blue');
-          $('ul').append(li);
-          $(li).append(linkHolder);
-
-        }
-          if (streamActive !== null) {
+        let streamActive = data.stream;
+        if (streamActive === null) {
+          let li = $('<li><a ' + link + '>' + user + '</br>' + 'Status - Offline</a></li>');
+          li.addClass('grey');
+          li.attr('href', link);
+          ul.append(li);
+        } else {
             let whatGame = data.stream.game;
             let status = data.stream.channel.status;
-            li = $('<li>' + user + '</br>' + 'Status - Online</br></li>');
-            $(li).addClass('green');
-            $('ul').append(li);
             let info = 'Game: ' + whatGame + '</br>' + 'Show: ' + status;
-            $(li).append(info);
-            $(li).append(linkHolder);
+            let li = $('<li><a ' + link + '>' + user + '</br>' + 'Status - Online</br>' + info + '</a></li>');
+            li.addClass('purple');
+            ul.append(li);
             console.log(whatGame);
             console.log(status);
-
           }
-          console.log(data);
-        });
-        listIdNumber++;
+        console.log(data);
       });
+    });
   });
- //hatClass('li');
 });
