6f50257e8f8575b519d24e18a3d3ce176fe1beb2
Home <elias.norstromm@gmail.com>
2018-05-29 21:41:26 +0100

    refactoring and comments

--- twitch.js
+++ twitch.js
@@ -2,28 +2,31 @@
 $(document).ready(function() {
   console.log('Ready');
 
-  let updateColorClass = function(buttonId) {
-    let inactiveButtons = $(buttonId).siblings();
+  // Update the css color class on the active and inactive buttons
+  let updateButtonColorClass = function(buttonId) {
     let activeButton = $(buttonId);
-    inactiveButtons.removeClass('active').addClass('inactive');
+    let inactiveButtons = $(buttonId).siblings();
     activeButton.removeClass('inactive').addClass('active');
+    inactiveButtons.removeClass('active').addClass('inactive');
   };
 
+  // Buttons to display 'all', 'online' and 'offline'
+  // channels respectively, and update the button colors
   $('#all').click(function() {
     $('li').show();
-    updateColorClass(this);
+    updateButtonColorClass(this);
   });
 
   $('#online').click(function() {
     $('.grey').hide();
     $('.purple').show();
-    updateColorClass(this);
+    updateButtonColorClass(this);
   });
 
   $('#offline').click(function() {
     $('.purple').hide();
     $('.grey').show();
-    updateColorClass(this);
+    updateButtonColorClass(this);
   });
 
   let ul = $('#list');
@@ -32,30 +35,31 @@ $(document).ready(function() {
 
   USERS.forEach(function(user) {
 
-    $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + user +  '?callback=?',  function(data) {
-      let url = data.url;
+    // Request to get the url for the current user going through the forEach
+    $.getJSON('https://wind-bow.gomix.me/twitch-api/channels/' + user +  '?callback=?',  function(data1) {
+      let url = data1.url;
       let link = "href=" + url + ' ' + 'target="_blank"';
-      console.log(data);
-      console.log(link);
 
-      $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?',  function(data) {
-        let streamActive = data.stream;
-        if (streamActive === null) {
-          let li = $('<li><a ' + link + '>' + user + '</br>' + 'Status - Offline</a></li>');
-          li.addClass('grey');
-          li.attr('href', link);
-          ul.append(li);
+      // Request to get stream status for current user
+      $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/' + user + '?callback=?',  function(data2) {
+        let streamActive = data2.stream;
+
+        // If the stream is active, get additional info on which show and what game is streaming
+        if (streamActive) {
+          let whatGame = data2.stream.game;
+          let status = data2.stream.channel.status;
+          let info = 'Game: ' + whatGame + '</br>' + 'Show: ' + status;
+
+          // Create an 'activeListItem' with color class 'purple' and append to the ul
+          let activeListItem = $('<li><a ' + link + '>' + user + '</br>' + 'Status - Online</br>' + info + '</a></li>');
+          activeListItem.addClass('purple');
+          ul.append(activeListItem);
         } else {
-            let whatGame = data.stream.game;
-            let status = data.stream.channel.status;
-            let info = 'Game: ' + whatGame + '</br>' + 'Show: ' + status;
-            let li = $('<li><a ' + link + '>' + user + '</br>' + 'Status - Online</br>' + info + '</a></li>');
-            li.addClass('purple');
-            ul.append(li);
-            console.log(whatGame);
-            console.log(status);
+          // If stream is inactive, create 'inactiveListItem' with color class 'grey' and append to ul
+            let inactiveListItem = $('<li><a ' + link + '>' + user + '</br>' + 'Status - Offline</a></li>');
+            inactiveListItem.addClass('grey');
+            ul.append(inactiveListItem);
           }
-        console.log(data);
       });
     });
   });
